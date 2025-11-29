36798c083b1641e43448df6fb3f09eba5ff95e33
yixunlim <limyixun@gmail.com>
2025-11-29 23:03:15 +0800

    Update cards_tab.py

--- cards_tab.py
+++ cards_tab.py
@@ -1,3 +1,6 @@
+import streamlit as st
+import pandas as pd
+
 CARDS_SHEET_URL = "https://docs.google.com/spreadsheets/d/1_VbZQuf86KRU062VfyLzPU6KP8C3XhZ7MPAvqjfjf0o/export?format=csv&id=1_VbZQuf86KRU062VfyLzPU6KP8C3XhZ7MPAvqjfjf0o&gid=0"
 
 @st.cache_data
@@ -23,3 +26,4 @@ def load_cards():
 
 df = load_cards()
 st.dataframe(df)
+
